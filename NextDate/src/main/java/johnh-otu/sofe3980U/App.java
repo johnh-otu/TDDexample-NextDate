473cafece83f91e464b02c20d06fbed316e24e57
John Howe <94157812+johnh-otu@users.noreply.github.com>
2024-02-20 18:15:37 -0500

    initialize maven project

---
+++ NextDate/src/main/java/johnh-otu/sofe3980U/App.java
@@ -0,0 +1,13 @@
+package johnh-otu.sofe3980U;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
