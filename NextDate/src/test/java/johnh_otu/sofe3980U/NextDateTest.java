db62e16d43df9b5d54b38145b3e80beefb991346
John Howe <94157812+johnh-otu@users.noreply.github.com>
2024-02-25 15:06:09 -0500

    create tests for valid inputs

--- NextDate/src/test/java/johnh_otu/sofe3980U/NextDateTest.java
+++ NextDate/src/test/java/johnh_otu/sofe3980U/NextDateTest.java
@@ -94,4 +94,77 @@ public class NextDateTest {
                 }
         );
     }
+
+    @Test @Tag("valid_inputs")
+    public void test1() {
+        int[] out = NextDate.NextDate(19, 2, 2023);
+        assertAll("test1",
+                () -> assertEquals(20, out[0]),
+                () -> assertEquals(2, out[1]),
+                () -> assertEquals(2023, out[2])
+        );
+    }
+    @Test @Tag("valid_inputs")
+    public void test2() {
+        int[] out = NextDate.NextDate(19, 6, 2024);
+        assertAll("test2",
+                () -> assertEquals(20, out[0]),
+                () -> assertEquals(6, out[1]),
+                () -> assertEquals(2024, out[2])
+        );
+    }
+    @Test @Tag("valid_inputs")
+    public void test3() {
+        int[] out = NextDate.NextDate(19, 10, 2023);
+        assertAll("test3",
+                () -> assertEquals(20, out[0]),
+                () -> assertEquals(10, out[1]),
+                () -> assertEquals(2023, out[2])
+        );
+    }
+    @Test @Tag("valid_inputs")
+    public void test4() {
+        int[] out = NextDate.NextDate(19, 12, 2024);
+        assertAll("test4",
+                () -> assertEquals(20, out[0]),
+                () -> assertEquals(12, out[1]),
+                () -> assertEquals(2024, out[2])
+        );
+    }
+    @Test @Tag("valid_inputs")
+    public void test5() {
+        int[] out = NextDate.NextDate(30, 6, 2023);
+        assertAll("test5",
+                () -> assertEquals(1, out[0]),
+                () -> assertEquals(7, out[1]),
+                () -> assertEquals(2023, out[2])
+        );
+    }
+    @Test @Tag("valid_inputs")
+    public void test6() {
+        int[] out = NextDate.NextDate(31, 10, 2024);
+        assertAll("test6",
+                () -> assertEquals(1, out[0]),
+                () -> assertEquals(11, out[1]),
+                () -> assertEquals(2024, out[2])
+        );
+    }
+    @Test @Tag("valid_inputs")
+    public void test7() {
+        int[] out = NextDate.NextDate(31, 12, 2023);
+        assertAll("test7",
+                () -> assertEquals(1, out[0]),
+                () -> assertEquals(1, out[1]),
+                () -> assertEquals(2024, out[2])
+        );
+    }
+    @Test @Tag("valid_inputs")
+    public void test8() {
+        int[] out = NextDate.NextDate(29, 2, 2024);
+        assertAll("test8",
+                () -> assertEquals(1, out[0]),
+                () -> assertEquals(3, out[1]),
+                () -> assertEquals(2024, out[2])
+        );
+    }
 }
