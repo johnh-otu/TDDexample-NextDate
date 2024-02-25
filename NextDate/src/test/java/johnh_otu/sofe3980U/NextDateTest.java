79d3f814d2d3da851e2ca6f5633d5f7f921f1fae
John Howe <94157812+johnh-otu@users.noreply.github.com>
2024-02-25 15:44:49 -0500

    extra cleanup for test file

--- NextDate/src/test/java/johnh_otu/sofe3980U/NextDateTest.java
+++ NextDate/src/test/java/johnh_otu/sofe3980U/NextDateTest.java
@@ -15,21 +15,15 @@ public class NextDateTest {
     public void negativeInputs() {
         assertAll("negative_inputs",
                 () -> {
-                    Exception e = assertThrows(IllegalArgumentException.class, () -> {
-                        NextDate.NextDate(-3, 2, 2023);
-                    });
+                    Exception e = assertThrows(IllegalArgumentException.class, () -> NextDate.nextDate(-3, 2, 2023));
                     assertEquals(daymsg, e.getMessage());
                 },
                 () -> {
-                    Exception e = assertThrows(IllegalArgumentException.class, () -> {
-                        NextDate.NextDate(19, -4, 2023);
-                    });
+                    Exception e = assertThrows(IllegalArgumentException.class, () -> NextDate.nextDate(19, -4, 2023));
                     assertEquals(monmsg, e.getMessage());
                 },
                 () -> {
-                    Exception e = assertThrows(IllegalArgumentException.class, () -> {
-                        NextDate.NextDate(19, 2, -5);
-                    });
+                    Exception e = assertThrows(IllegalArgumentException.class, () -> NextDate.nextDate(19, 2, -5));
                     assertEquals(yearmsg, e.getMessage());
                 }
         );
@@ -39,21 +33,15 @@ public class NextDateTest {
     public void zeroInputs() {
         assertAll("zero_inputs",
                 () -> {
-                    Exception e = assertThrows(IllegalArgumentException.class, () -> {
-                        NextDate.NextDate(0, 2, 2023);
-                    });
+                    Exception e = assertThrows(IllegalArgumentException.class, () -> NextDate.nextDate(0, 2, 2023));
                     assertEquals(daymsg, e.getMessage());
                 },
                 () -> {
-                    Exception e = assertThrows(IllegalArgumentException.class, () -> {
-                        NextDate.NextDate(19, 0, 2023);
-                    });
+                    Exception e = assertThrows(IllegalArgumentException.class, () -> NextDate.nextDate(19, 0, 2023));
                     assertEquals(monmsg, e.getMessage());
                 },
                 () -> {
-                    Exception e = assertThrows(IllegalArgumentException.class, () -> {
-                        NextDate.NextDate(19, 2, 0);
-                    });
+                    Exception e = assertThrows(IllegalArgumentException.class, () -> NextDate.nextDate(19, 2, 0));
                     assertEquals(yearmsg, e.getMessage());
                 }
         );
@@ -63,21 +51,15 @@ public class NextDateTest {
     public void positiveInvalidInputs() {
         assertAll("pos_invalid_inputs",
                 () -> {
-                    Exception e = assertThrows(IllegalArgumentException.class, () -> {
-                        NextDate.NextDate(29, 2, 2023);
-                    });
+                    Exception e = assertThrows(IllegalArgumentException.class, () -> NextDate.nextDate(29, 2, 2023));
                     assertEquals(daymsg, e.getMessage());
                 },
                 () -> {
-                    Exception e = assertThrows(IllegalArgumentException.class, () -> {
-                        NextDate.NextDate(19, 15, 2023);
-                    });
+                    Exception e = assertThrows(IllegalArgumentException.class, () -> NextDate.nextDate(19, 15, 2023));
                     assertEquals(monmsg, e.getMessage());
                 },
                 () -> {
-                    Exception e = assertThrows(IllegalArgumentException.class, () -> {
-                        NextDate.NextDate(19, 2, 2213);
-                    });
+                    Exception e = assertThrows(IllegalArgumentException.class, () -> NextDate.nextDate(19, 2, 2213));
                     assertEquals(yearmsg, e.getMessage());
                 }
         );
@@ -85,7 +67,7 @@ public class NextDateTest {
 
     @Test @Tag("valid_inputs")
     public void test1() {
-        int[] out = NextDate.NextDate(19, 2, 2023);
+        int[] out = NextDate.nextDate(19, 2, 2023);
         assertAll("test1",
                 () -> assertEquals(20, out[0]),
                 () -> assertEquals(2, out[1]),
@@ -94,7 +76,7 @@ public class NextDateTest {
     }
     @Test @Tag("valid_inputs")
     public void test2() {
-        int[] out = NextDate.NextDate(19, 6, 2024);
+        int[] out = NextDate.nextDate(19, 6, 2024);
         assertAll("test2",
                 () -> assertEquals(20, out[0]),
                 () -> assertEquals(6, out[1]),
@@ -103,7 +85,7 @@ public class NextDateTest {
     }
     @Test @Tag("valid_inputs")
     public void test3() {
-        int[] out = NextDate.NextDate(19, 10, 2023);
+        int[] out = NextDate.nextDate(19, 10, 2023);
         assertAll("test3",
                 () -> assertEquals(20, out[0]),
                 () -> assertEquals(10, out[1]),
@@ -112,7 +94,7 @@ public class NextDateTest {
     }
     @Test @Tag("valid_inputs")
     public void test4() {
-        int[] out = NextDate.NextDate(19, 12, 2024);
+        int[] out = NextDate.nextDate(19, 12, 2024);
         assertAll("test4",
                 () -> assertEquals(20, out[0]),
                 () -> assertEquals(12, out[1]),
@@ -121,7 +103,7 @@ public class NextDateTest {
     }
     @Test @Tag("valid_inputs")
     public void test5() {
-        int[] out = NextDate.NextDate(30, 6, 2023);
+        int[] out = NextDate.nextDate(30, 6, 2023);
         assertAll("test5",
                 () -> assertEquals(1, out[0]),
                 () -> assertEquals(7, out[1]),
@@ -130,7 +112,7 @@ public class NextDateTest {
     }
     @Test @Tag("valid_inputs")
     public void test6() {
-        int[] out = NextDate.NextDate(31, 10, 2024);
+        int[] out = NextDate.nextDate(31, 10, 2024);
         assertAll("test6",
                 () -> assertEquals(1, out[0]),
                 () -> assertEquals(11, out[1]),
@@ -139,7 +121,7 @@ public class NextDateTest {
     }
     @Test @Tag("valid_inputs")
     public void test7() {
-        int[] out = NextDate.NextDate(31, 12, 2023);
+        int[] out = NextDate.nextDate(31, 12, 2023);
         assertAll("test7",
                 () -> assertEquals(1, out[0]),
                 () -> assertEquals(1, out[1]),
@@ -148,7 +130,7 @@ public class NextDateTest {
     }
     @Test @Tag("valid_inputs")
     public void test8() {
-        int[] out = NextDate.NextDate(29, 2, 2024);
+        int[] out = NextDate.nextDate(29, 2, 2024);
         assertAll("test8",
                 () -> assertEquals(1, out[0]),
                 () -> assertEquals(3, out[1]),
